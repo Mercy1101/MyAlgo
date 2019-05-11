538c7576bc5c1f4c2ae48d870fb231fd86fbbe94
Mercy1101 <pipinstall@163.com>
2019-05-11 22:20:41 +0800

    update

--- MyAlgo/main.cpp
+++ MyAlgo/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "gtest/gtest.h"
 #include "easylogging++.h"
-#include "list.h"
+#include "profiler.h"
 
 INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char **argv)
 {
+    TinySTL::profiler::ProfilerInstance::start();
     ::testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
     LOG(INFO) << "Test once!";
+    TinySTL::profiler::ProfilerInstance::finish();
+    TinySTL::profiler::ProfilerInstance::dumpDuringTime();
+    TinySTL::profiler::ProfilerInstance::memory();
     return 0;
 }
