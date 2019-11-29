3f30b687514441022bafa27338db8c74ee1d8060
Mercy1101 <pipinstall@163.com>
2019-11-29 14:02:08 +0800

    增加了leetcode的解法文件和测试文件

---
+++ src/leetcode/leetcode.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+#include <vector>
+#include "leetcode.h"
+
+namespace Lee { namespace Leetcode{
+
+    bool isPalindrome(std::string s)
+    {
+        if (s.empty()) return false;
+        if (s.length() == 1 && !::isalnum(*s.begin())) return false;
+        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+        auto left = s.begin();
+        auto right = std::prev(s.end());
+        while (left < right)
+        {
+            if (!::isalnum(*left)) ++left;
+            else if (!::isalnum(*right)) --right;
+            else if (*left != *right) return false;
+            else
+            {
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
+
+    bool IsValidParentheses (std::string const& s)
+    {
+        if (s.empty()) return false;
+
+        std::string left = "([{";
+        std::string right = ")]}";
+        std::stack<char> stk;
+        for (const auto& c : s)
+        {
+            if (left.find(c) != std::string::npos)
+            {
+                stk.push (c);
+            }
+            else
+            {
+                if (stk.empty () || stk.top () != left.at(right.find (c))) return false;
+                else stk.pop ();
+            }
+        }
+        return stk.empty();
+    }
+
+
+
+}   // end of namespace Leetcode
+}   // end of namespace Lee
---
+++ unittest/leetcode/leetcode_unittest.cpp
@@ -0,0 +1,35 @@
+﻿#include <gtest/gtest.h>
+#include "leetcode.h"
+
+/** 
+ * @brief Lee::Leetcode::isPalindrome()函数的简单测试
+ */
+TEST(isPalindrome, Simple)
+{
+    using namespace Lee;
+    EXPECT_TRUE(Leetcode::isPalindrome("a"));
+    EXPECT_TRUE(Leetcode::isPalindrome("aba"));
+    EXPECT_TRUE(Leetcode::isPalindrome("ABa"));
+    EXPECT_TRUE(Leetcode::isPalindrome("A man, a plan, a canal: Panama"));
+    
+    EXPECT_FALSE(Leetcode::isPalindrome(","));
+    EXPECT_FALSE(Leetcode::isPalindrome(""));
+    EXPECT_FALSE(Leetcode::isPalindrome("race a car"));
+}
+
+/** 
+ * @brief Lee::Leetcode::IsValidParentheses()函数的简单测试
+ */
+TEST(IsValidParentheses, Simple)
+{
+    using namespace Lee;
+    EXPECT_TRUE(Leetcode::IsValidParentheses("((){{}([[]{[]}]())})"));
+    EXPECT_TRUE(Leetcode::IsValidParentheses("()"));
+    EXPECT_TRUE(Leetcode::IsValidParentheses("()[]"));
+
+    EXPECT_FALSE(Leetcode::IsValidParentheses(""));
+    EXPECT_FALSE(Leetcode::IsValidParentheses("(]"));
+    EXPECT_FALSE(Leetcode::IsValidParentheses("([)]"));
+    EXPECT_FALSE(Leetcode::IsValidParentheses("("));
+    EXPECT_FALSE(Leetcode::IsValidParentheses(")"));
+}
