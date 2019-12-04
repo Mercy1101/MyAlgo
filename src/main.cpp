d39c5fd8d87605fed464b8b0455fe3be94745405
Mercy1101 <pipinstall@163.com>
2019-12-04 17:39:59 +0800

    gtest被替换成了Catch2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-﻿#include <iostream>
+﻿#define CATCH_CONFIG_RUNNER
+#include <iostream>
 
 #include "Version/Version.h"  // for print PrintVersion()
-#include "gtest/gtest.h"
+#include "catch2/catch.hpp"   // include after defining CATCH_CONFIG_RUNNER
+//#include "gtest/gtest.h"
 #include "init/init.h"
 #include "utility/utility.h"
 
@@ -9,21 +11,24 @@ int main(int argc, char** argv) {
   Lee::ignore_unused(argc, argv);
   PrintVersion();
   CheckConfigFolder();
+  //
+  ///** 测试模式下跑单元测试 */
+  //#ifdef _DEBUG
+  //  testing::InitGoogleTest(&argc, argv);
+  //#ifdef TXT_OUTPUT
+  //  // Gets hold of the event listener list.
+  //  ::testing::TestEventListeners& listeners =
+  //      ::testing::UnitTest::GetInstance()->listeners();
+  //  delete listeners.Release(listeners.default_result_printer());
+  //  // Adds a listener to the end.  googletest takes the ownership.
+  //  listeners.Append(new MinimalistPrinter("./" + getCurrentTimeStr() +
+  //  ".txt"));
+  //#endif
+  //  RUN_ALL_TESTS();
+  //#endif
 
-/** 测试模式下跑单元测试 */
-#ifdef _DEBUG
-  testing::InitGoogleTest(&argc, argv);
-#ifdef TXT_OUTPUT
-  // Gets hold of the event listener list.
-  ::testing::TestEventListeners& listeners =
-      ::testing::UnitTest::GetInstance()->listeners();
-  delete listeners.Release(listeners.default_result_printer());
-  // Adds a listener to the end.  googletest takes the ownership.
-  listeners.Append(new MinimalistPrinter("./" + getCurrentTimeStr() + ".txt"));
-#endif
-  RUN_ALL_TESTS();
-#endif
-
+  /// catch2的单元测试
+  Catch::Session().run(argc, argv);
   system("pause");
   return 0;
 }
