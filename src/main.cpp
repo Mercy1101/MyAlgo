7ede63673de40d0b16d523a4cad3a3b60d8d6107
Mercy1101 <pipinstall@163.com>
2019-12-06 15:38:59 +0800

    整理了格式

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,12 @@
 
 #include "Version/Version.h"  // for print PrintVersion()
 #include <catch2/catch.hpp>   // include after defining CATCH_CONFIG_RUNNER
-//#include "gtest/gtest.h"
 #include "init/init.h"
 #include "utility/utility.h"
 
 int main(int argc, char** argv) {
-  Lee::ignore_unused(argc, argv);
   PrintVersion();
   CheckConfigFolder();
-
-  /// catch2的单元测试
   Catch::Session().run(argc, argv);
-  system("pause");
   return 0;
 }
