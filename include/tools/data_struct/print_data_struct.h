37e89dc9571032775843807957916e4e8b91f0b2
Mercy1101 <pipinstall@163.com>
2019-12-10 16:42:55 +0800

    添加了print_data_struct.cpp文件和它的单元测试文件.里面添加了函数打印固定深度的完全二叉树.

---
+++ include/tools/data_struct/print_data_struct.h
@@ -0,0 +1,35 @@
+﻿///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+/// Copyright (c) 2019, FRITT Inc. All rights reserved.
+///
+/// @file   print_data_struct.h
+/// @brief  这个文件主要写如何打印数据结构的函数
+///
+/// @author lijiancong, pipinstall@163.com
+/// @date   2019-12-10 09:02:14
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+#ifndef MYALGO_INCLUDE_TOOLS_DATASTRUCT_PRINT_DATA_STRUCT_H_
+#define MYALGO_INCLUDE_TOOLS_DATASTRUCT_PRINT_DATA_STRUCT_H_
+
+namespace Lee {
+inline namespace data_struct {
+inline namespace print_data_struct {
+/// @name     PrintCompleteTree
+/// @brief    打印一个完整的树，树的深度不超过5
+///
+/// @param    depth [in]  树的深度
+///
+/// @return   NONE
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2019-12-10 13:53:10
+/// @warning  线程不安全
+void PrintCompleteTree(const int &depth);
+
+
+}  // namespace print_data_struct
+}  // namespace data_struct
+}  // namespace Lee
+
+
+#endif  // end of MYALGO_INCLUDE_TOOLS_DATASTRUCT_PRINT_DATA_STRUCT_H_
---
+++ src/tools/data_struct/print_data_struct.cpp
@@ -0,0 +1,60 @@
+﻿#include "data_struct/print_data_struct.h"
+#include <iostream>
+#include "utility/utility.h"
+
+namespace Lee {
+inline namespace data_struct {
+inline namespace print_data_struct {
+const char g_letter[32] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+                           'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                           'o', 'p', 'q', 'r', 's', 't', 'u', 'v'};
+
+void PrintSpace(const int &space_number) {
+  for (int i = 0; i < space_number - 1; ++i) {
+    std::cout << ' ';
+  }
+}
+void PrintElement(const int &element_number) {
+  if (element_number > 31) {
+    PrintElement(element_number - 31);
+    return;
+  }
+  std::cout << g_letter[element_number];
+}
+
+void PrintLine(const int &line_number) {
+  for (int i = 0; i < line_number - 1; ++i) {
+    std::cout << '_';
+  }
+}
+
+void PrintCompleteTree(const int &depth) {
+  std::cout << "\n";
+  if (depth == 0 || depth > 6) {
+    std::cout << "Invalid Depth Param!"
+              << "\n";
+    return;
+  }
+
+  for (int i = 0; i < depth + 1; ++i) {  /// 深度循环
+    int element_count = Lee::power(2, i);
+    int last_offset = 0;
+    for (int count = 0; count < element_count; ++count) {  /// 单行元素循环
+      auto off_set = (((count + 1) * 2) - 1) * Lee::power(2, (depth - i));
+      if (count != 0) {
+        PrintLine(off_set - last_offset);
+      } else {
+        PrintSpace(off_set - last_offset);
+      }
+      last_offset = off_set;
+      PrintElement(count);
+    }
+    std::cout << "\n";
+    std::cout << "\n";
+  }
+  std::cout << "\n";
+}
+}  // namespace print_data_struct
+}  // namespace data_struct
+}  // namespace Lee
