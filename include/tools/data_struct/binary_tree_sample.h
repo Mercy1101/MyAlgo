fc43b3974f7748665c4456d7d037f6ba8ebc5407
Mercy1101 <pipinstall@163.com>
2019-12-21 20:24:37 +0800

    添加了avl_tree_sample.h和binary_tree_sample.h两个文件，接下来开始写binary_tree_sample的测试用例

---
+++ include/tools/data_struct/binary_tree_sample.h
@@ -0,0 +1,196 @@
+﻿///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+/// Copyright (c) 2019, FRITT Inc. All rights reserved.
+///
+/// @file   binary_tree_sample.h
+/// @brief  二叉树示例，
+///         从<Data Structures And Algorithm Analysis In Cpp>书中抄来的
+///
+/// @author lijiancong, pipinstall@163.com
+/// @date   2019-12-21 19:42:22
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+#ifndef MYALGO_INCLUDE_TOOLS_DATASTRUCT_BINARY_TREE_SAMPLE_H_
+#define MYALGO_INCLUDE_TOOLS_DATASTRUCT_BINARY_TREE_SAMPLE_H_
+
+#include <atomic>
+#include <iostream>
+
+namespace Lee {
+inline namespace data_struct {
+inline namespace binary_tree {
+
+template <typename Comparable>
+class BinarySearchTree {
+ public:
+  BinarySearchTree() : root{nullptr};
+  /**
+   * Copy constructor
+   */
+  BinarySearchTree(const BinarySearchTree &rhs) : root{nullptr} {
+    root = clone(rhs.root);
+  }
+  BinarySearchTree(BinarySearchTree &&rhs) : root{nullptr} {
+    root = clone(rhs.root);
+  }
+
+  ~BinarySearchTree() { makeEmpty(); }
+
+  const Comparable &findMin() const { findMin(root); }
+  const Comparable &findMax() const { findMax(root); }
+  /**
+   * Returns true if x is found in the tree.
+   */
+  bool contains(const Comparable &x) const { return contains(x, root); }
+
+  bool isEmpty() const { return nullptr == root; };
+  void printTree(std::ostream &out = std::cout) const { printTree(root, out); }
+
+  void makeEmpty() { makeEmpty(root) }
+  /**
+   * Insert x into the tree; duplicates are ignored.
+   */
+  void insert(const Comparable &x) { insert(x, root); }
+
+  void insert(Comparable &&x) { insert(x, root); }
+  /**
+   * Remove x from the tree. Nothing is done if x is not found.
+   */
+  void remove(const Comparable &x) { remove(x, root); }
+
+  BinarySearchTree &operator=(const BinarySearchTree &rhs);
+  BinarySearchTree &operator=(BinarySearchTree &&rhs);
+
+ private:
+  struct BinaryNode {
+    Comparable element;
+    BinaryNode *left;
+    BinaryNode *right;
+
+    BinaryNode(const Comparable &theElement, BinaryNode *lt, BinaryNode *rt)
+        : element{theElement}, left{lt}, right{rt} {}
+
+    BinaryNode(Comparable &&theElement, BinaryNode *lt, BinaryNode *rt)
+        : element{std::move(theElement)}, left{lt}, right{rt} {}
+  };
+
+  BinaryNode *root;
+
+  /**
+   * Internal method to insert into a subtree.
+   * x is the item to insert.
+   * t is the node that roots the subtree.
+   * Set the new root of the subtree.
+   */
+  void insert(const Comparable &x, BinaryNode *&t) {
+    if (t == nullptr)
+      t = new BinaryNode{x, nullptr, nullptr};
+    else if (x < t->element)
+      insert(x, t->left);
+    else if (t->element < x)
+      insert(x, t->right);
+    else
+      ;  // Duplicate; do nothing
+  }
+
+  /**
+   * Internal method to insert into a subtree.
+   * x is the item to insert by moving.
+   * t is the node that roots the subtree.
+   * Set the new root of the subtree.
+   */
+  void insert(Comparable &&x, BinaryNode *&t) {
+    if (t == nullptr)
+      t = new BinaryNode{std::move(x), nullptr, nullptr};
+    else if (x < t->element)
+      insert(std::move(x), t->left);
+    else if (t->element < x)
+      insert(std::move(x), t->right);
+    else
+      ;  // Duplicate; do nothing
+  }
+  /**
+   * Internal method to remove from a subtree.
+   * x is the item to remove.
+   * t is the node that roots the subtree.
+   * Set the new root of the subtree.
+   */
+  void remove(const Comparable &x, BinaryNode *&t) {
+    if (t == nullptr) return;  // Item not found; do nothing
+    if (x < t->element)
+      remove(x, t->left);
+    else if (t->element < x)
+      remove(x, t->right);
+    else if (t->left != nullptr && t->right != nullptr)  // Two children
+    {
+      t->element = findMin(t->right)->element;
+      remove(t->element, t->right);
+    } else {
+      BinaryNode *oldNode = t;
+      t = (t->left != nullptr) ? t->left : t->right;
+      delete oldNode;
+    }
+  }
+
+  /**
+   * Internal method to find the smallest item in a subtree t.
+   * Return node containing the smallest item.
+   */
+  BinaryNode *findMin(BinaryNode *t) const {
+    if (t == nullptr) return nullptr;
+    if (t->left == nullptr) return t;
+    return findMin(t->left);
+  }
+  /**
+   * Internal method to find the largest item in a subtree t.
+   * Return node containing the largest item.
+   */
+  BinaryNode *findMax(BinaryNode *t) const {
+    if (t != nullptr)
+      while (t->right != nullptr) t = t->right;
+    return t;
+  }
+  /**
+   * Internal method to test if an item is in a subtree.
+   * x is item to search for.
+   * t is the node that roots the subtree.
+   */
+  bool contains(const Comparable &x, BinaryNode *t) const {
+    if (t == nullptr)
+      return false;
+    else if (x < t->element)
+      return contains(x, t->left);
+    else if (t->element < x)
+      return contains(x, t->right);
+    else
+      return true;  // Match
+  }
+  /**
+   * Internal method to make subtree empty.
+   */
+  void makeEmpty(BinaryNode *&t) {
+    if (t != nullptr) {
+      makeEmpty(t->left);
+      makeEmpty(t->right);
+      delete t;
+    }
+    t = nullptr;
+  }
+  void printTree(BinaryNode *t, std::ostream &out) const;
+
+  /**
+   * Internal method to clone subtree.
+   */
+  BinaryNode *clone(BinaryNode *t) const {
+    if (t == nullptr) {
+      return nullptr;
+    } else {
+      return new BinaryNode{t->element, clone(t->left), clone(t->right)};
+    }
+  }
+};
+
+}  // namespace binary_tree
+}  // namespace data_struct
+}  // namespace Lee
+
+#endif  // end of MYALGO_INCLUDE_TOOLS_DATASTRUCT_BINARY_TREE_SAMPLE_H_
---
+++ unittest/tools/data_struct/binary_tree_sample_unittest.cpp
@@ -0,0 +1,10 @@
+﻿#include <binary_tree_sample.h>
+
+#include <catch2/catch.hpp>
+
+
+namespace Lee {
+inline namespace data_struct {
+inline namespace binary_tree {}  // namespace binary_tree
+}  // namespace data_struct
+}  // namespace Lee
