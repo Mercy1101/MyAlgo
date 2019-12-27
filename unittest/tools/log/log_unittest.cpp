df0dc83ffb6c0a530d773502e893ec14fa83015e
Mercy1101 <pipinstall@163.com>
2019-12-27 10:45:36 +0800

    添加了日志模块的多线程打印测试用例，和定义了日志测试的宏

--- unittest/tools/log/log_unittest.cpp
+++ unittest/tools/log/log_unittest.cpp
@@ -1,13 +1,18 @@
-#include "log/log.h"
+﻿#include "log/log.h"
 
+#include <atomic>
 #include <catch2/catch.hpp>
 #include <iostream>
+#include "utility/utility.h"
 
+// #define LOG_UNITTEST
+#undef LOG_UNITTEST
+#ifdef LOG_UNITTEST
 /**
  * @brief 对log模块进行简单的单元测试
  */
 TEST_CASE("简单的打印测试", "[log]") {
-  #if 0
+
   LOG(LOG_TRACE, "This is Unittest!");
   LOG(LOG_DEBUG, "This is Unittest!");
   LOG(LOG_INFO, "This is Unittest!");
@@ -24,7 +29,21 @@ TEST_CASE("简单的打印测试", "[log]") {
   LOG_NO_FUNCTIONNAME(LOG_ERROR, "This is Unittest!");
   LOG_NO_FUNCTIONNAME(LOG_CRITICAL, "This is Unittest!");
   LOG_NO_FUNCTIONNAME(LOG_OFF, "This is Unittest!");
-  #endif
+
   /** 不支持变参，只支持string */
   // LOG_NO_FUNCTIONNAME(SPD_INFO, "%s,%d,%x", "This is Unittest!", 1024, 256);
 }
+
+std::atomic<int> count = 0;
+TEST_CASE("测试多线程下异步日志的效果", "[log]") {
+  for (int i = 0; i < 100; ++i)
+  {
+    std::thread ls([i]() {
+      for (int xx = 0; xx < 100; ++xx)
+        LOG(LOG_INFO, "this is count(%d) thread(%d).", ++count, i);
+      Lee::SleepForRandomMilliSecond(2);
+                   });
+    ls.detach();
+  }
+}
+#endif // end of LOG_UNITTEST
