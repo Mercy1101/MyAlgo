326b75d5c0a2b029a4de490bc9ed63975ab3b2ab
Mercy1101 <pipinstall@163.com>
2019-12-29 18:16:15 +0800

    二叉树的测试通过了构造函数，处置列表构造函数，插入函数

--- include/tools/data_struct/binary_tree.h
+++ include/tools/data_struct/binary_tree.h
@@ -32,42 +32,7 @@ inline namespace binary_tree {
 /// @warning  线程不安全
 template <typename Comparable>
 class Binary_Tree {
-  using Position = struct BinaryTreeNode *;
-  using Tree = struct BinaryTreeNode *;
-
  public:
-  Binary_Tree(const Comparable &data) : root_(new BinaryTreeNode(data)) {}
-  Binary_Tree(const std::initializer_list<Comparable> &init_list) {
-    ++construct_count;
-    root_ = new BinaryTreeNode<T>(*init_list.begin());
-    for (const auto &it : init_list) {
-      this->Insert(it);
-    }
-  }
-  ~Binary_Tree() { Destory<T>(root_); }
-
-  void Insert(const Comparable &data) { Insert(data, root_); }
-
- private:
-  void Insert(const Comparable &data, Tree tree) {
-    auto node_ptr = tree;
-    auto pre_node_ptr = tree;
-    while (node_ptr != nullptr) {
-      pre_node_ptr = node_ptr;
-      if (data > node_ptr->data) {
-        node_ptr = node_ptr->right;
-      } else if (data < node_ptr->data) {
-        node_ptr = node_ptr->left;
-      } else {
-        /// 相等的情况什么也不做
-        return;
-      }
-    }
-    node_ptr = new BinaryTreeNode(data, pre_node_ptr);
-  }
-  Destory();
-
- private:
   struct BinaryTreeNode {
     BinaryTreeNode(const Comparable &data, BinaryTreeNode *parent)
         : data(data), left(nullptr), right(nullptr), parent(parent) {
@@ -85,7 +50,113 @@ class Binary_Tree {
     /// 用于构造时自加, 析构时自减
     inline static std::atomic<int> construct_count = 0;
   };
+  using Position = struct BinaryTreeNode *;
+  using Tree = struct BinaryTreeNode *;
 
+  Binary_Tree(const Comparable &data) : root_(new BinaryTreeNode(data)) {}
+  Binary_Tree(const std::initializer_list<Comparable> &init_list) {
+    if (0 == init_list.size()) {
+      root_ = new BinaryTreeNode(0, nullptr);
+      return;
+    }
+    root_ = new BinaryTreeNode(*init_list.begin());
+    for (const auto &it : init_list) {
+      this->Insert(it);
+    }
+  }
+  ~Binary_Tree() { Destory(root_); }
+
+  void Insert(const Comparable &data) { Insert(data, root_); }
+  void Destory() { Destory(root_); }
+
+  Tree GetRoot() const { return root_; }
+
+ private:
+  /// @name     Insert
+  /// @brief    插入一个元素到树中
+  ///
+  /// @param    data [in]
+  /// @param    tree [in]
+  ///
+  /// @return   NONE
+  ///
+  /// @author   Lijiancong, pipinstall@163.com
+  /// @date     2019-12-29 13:20:11
+  /// @warning  线程不安全
+  void Insert(const Comparable &data, Tree tree) {
+    if (nullptr == tree) {
+      /// 这个树要是为空的话就不会做任何事
+      return;
+    }
+    auto node_ptr = tree;
+    auto pre_node_ptr = tree;
+    while (node_ptr != nullptr) {
+      pre_node_ptr = node_ptr;
+      if (data > node_ptr->data) {
+        node_ptr = node_ptr->right;
+      } else if (data < node_ptr->data) {
+        node_ptr = node_ptr->left;
+      } else {
+        /// 相等的情况什么也不做
+        return;
+      }
+    }
+
+    if (data > pre_node_ptr->data) {
+      pre_node_ptr->right = new BinaryTreeNode(data, pre_node_ptr);
+    } else if (data < pre_node_ptr->data) {
+      pre_node_ptr->left = new BinaryTreeNode(data, pre_node_ptr);
+    } else {
+      /// 相等什么也不做
+      return;
+    }
+    return;
+  }
+
+  /// @name     Destory
+  /// @brief    删除节点释放内存
+  ///
+  /// @param    tree  [in]
+  ///
+  /// @return   NONE
+  ///
+  /// @author   Lijiancong, pipinstall@163.com
+  /// @date     2019-12-29 14:40:02
+  /// @warning  线程不安全
+  void Destory(const Tree tree) const {
+    if (tree != nullptr) {
+      Destory(tree->left);
+      Destory(tree->right);
+      delete tree;
+    }
+    return;
+  }
+
+  /// @name     FindMin
+  /// @brief    找寻树中最小的元素
+  ///
+  /// @param
+  ///
+  /// @return
+  ///
+  /// @author   Lijiancong, pipinstall@163.com
+  /// @date     2019-12-29 18:14:11
+  /// @warning  线程不安全
+  Position FindMin() {}
+
+  /// @name     Delete
+  /// @brief    删除某特定元素
+  ///
+  /// @param
+  ///
+  /// @return
+  ///
+  /// @author   Lijiancong, pipinstall@163.com
+  /// @date     2019-12-29 18:11:41
+  /// @warning  线程不安全
+  void Delete() {}
+
+ private:
   Tree root_;
 };  // namespace binary_tree
 
--- unittest/tools/data_struct/binary_tree_unittest.cpp
+++ unittest/tools/data_struct/binary_tree_unittest.cpp
@@ -4,322 +4,72 @@
 
 #include "profiler/Profiler.h"
 
-SCENARIO("binary_tree, 二叉树的测试", "[data_struct][binary_tree]") {
-  GIVEN("构造函数测试") {
-    WHEN("创建一个二叉树") {
+SCENARIO("binary_tree 二叉树的构造函数测试", "[data_struct][binary_tree]") {
+  GIVEN("一个单独的元素来创建一个树") {
+    WHEN("数值类型为int时") {
       Lee::Binary_Tree<int> tree(1);
       THEN("root不为空指针, root节点的data则为1, 左边和右边都是空指针") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 1);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 1);
-        REQUIRE_FALSE(tree.GetRootNode() == nullptr);
-        REQUIRE(tree.GetRootNode()->data == 1);
-        REQUIRE(tree.GetRootNode()->left == nullptr);
-        REQUIRE(tree.GetRootNode()->right == nullptr);
-      }
-    }
-    WHEN("使用初始化链表来构造一个二叉树") {
+        REQUIRE(tree.GetRoot()->left == nullptr);
+        REQUIRE(tree.GetRoot()->right == nullptr);
+        REQUIRE(tree.GetRoot()->data == 1);
+      } /// THEN
+    } /// WHEN
+    REQUIRE(0 == Lee::Binary_Tree<int>::BinaryTreeNode::construct_count);
+  } /// GIVEN
+  GIVEN("一个初值列表来创建一个树") {
+    WHEN("只有一个元素的初值列表") {
       Lee::Binary_Tree<int> tree{1};
-      REQUIRE(tree.Size() == 1);
-      REQUIRE(tree.GetHeight() == 0);
-      REQUIRE(tree.GetRootNode()->data == 1);
-
-      Lee::Binary_Tree<int> tree2{5, 6, 1, 3, 4, 7, 8, 9, 10};
-      REQUIRE(tree2.Size() == 9);
-      REQUIRE(5 == tree2.GetHeight());
-      REQUIRE(tree2.GetRootNode()->data == 5);
-    }
-    WHEN("创建一个二叉树测试用例结束时") {
-      THEN("树本身和树节点, 应该被正确析构") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 0);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 0);
-      }
-    }
-  }
-  GIVEN("Insert函数测试") {
-    WHEN("创建一个二叉树, 并插入5个数值") {
-      Lee::Binary_Tree<int> insert_tree{1, 2, 3, 4, 5, 6};
-      THEN("这个树被实例化了一次, 节点不为空,高度为5,且元素个数为6") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 1);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 6);
-        REQUIRE_FALSE(insert_tree.GetRootNode() == nullptr);
-        REQUIRE(5 == insert_tree.GetHeight());
-        REQUIRE(6 == insert_tree.Size());
-      }
-      THEN("树中所有元素靠右, 分别为1, 2, 3, 4, 5, 6.所有左子树都为空") {
-        auto node_ptr = insert_tree.GetRootNode();
-        REQUIRE(1 == node_ptr->data);
-        REQUIRE(nullptr == node_ptr->left);
-        node_ptr = node_ptr->right;
-        REQUIRE(2 == node_ptr->data);
-        REQUIRE(nullptr == node_ptr->left);
-        node_ptr = node_ptr->right;
-        REQUIRE(3 == node_ptr->data);
-        REQUIRE(nullptr == node_ptr->left);
-        node_ptr = node_ptr->right;
-        REQUIRE(4 == node_ptr->data);
-        REQUIRE(nullptr == node_ptr->left);
-        node_ptr = node_ptr->right;
-        REQUIRE(5 == node_ptr->data);
-        REQUIRE(nullptr == node_ptr->left);
-      }
-    }
-    WHEN("Insert函数测试结束时") {
-      THEN("树本身和树节点, 应该被正确析构") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 0);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 0);
-      }
-    }
-
-    WHEN("创建一个二叉树, 并插入8个不重复数值") {
-      Lee::Binary_Tree<int> insert_tree{5, 2, 7, 1, 3, 6, 8, 4};
-      THEN("这个树被实例化了一次, 节点不为空,高度为3,且元素个数为8") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 1);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 8);
-        REQUIRE_FALSE(insert_tree.GetRootNode() == nullptr);
-        REQUIRE(3 == insert_tree.GetHeight());
-        REQUIRE(8 == insert_tree.Size());
-      }
-      THEN("树中所有元素的值, 分别为5,2,7,1,3,6,8,4") {
-        auto root_node_ptr = insert_tree.GetRootNode();
-        /// 根节点
-        REQUIRE(5 == root_node_ptr->data);
-        /// 第二层
-        REQUIRE(2 == root_node_ptr->left->data);
-        REQUIRE(7 == root_node_ptr->right->data);
-        /// 第三层
-        REQUIRE(1 == root_node_ptr->left->left->data);
-        REQUIRE(nullptr == root_node_ptr->left->left->left);
-        REQUIRE(nullptr == root_node_ptr->left->left->right);
-        REQUIRE(3 == root_node_ptr->left->right->data);
-        REQUIRE(nullptr == root_node_ptr->left->right->left);
-        REQUIRE(6 == root_node_ptr->right->left->data);
-        REQUIRE(nullptr == root_node_ptr->right->left->left);
-        REQUIRE(nullptr == root_node_ptr->right->left->right);
-        REQUIRE(8 == root_node_ptr->right->right->data);
-        REQUIRE(nullptr == root_node_ptr->right->right->left);
-        REQUIRE(nullptr == root_node_ptr->right->right->right);
-        /// 第四层
-        REQUIRE(4 == root_node_ptr->left->right->right->data);
-        REQUIRE(nullptr == root_node_ptr->left->right->right->left);
-        REQUIRE(nullptr == root_node_ptr->left->right->right->right);
-      }
-    }
-    WHEN("创建一个二叉树, 并插入8个不重复数值函数测试结束时") {
-      THEN("树本身和树节点, 应该被正确析构") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 0);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 0);
-      }
-    }
-    WHEN("创建一个二叉树, 并插入8个相同的数值") {
-      Lee::Binary_Tree<int> insert_tree{5, 5, 5, 5, 5, 5, 5, 5};
-      THEN("这个树被实例化了一次, 节点不为空, 高度为0,且元素个数为1") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 1);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 1);
-        REQUIRE_FALSE(insert_tree.GetRootNode() == nullptr);
-        REQUIRE(0 == insert_tree.GetHeight());
-        REQUIRE(1 == insert_tree.Size());
-      }
-      THEN("树中只有一个元素,其值为5") {
-        auto root_node_ptr = insert_tree.GetRootNode();
-        /// 根节点
-        REQUIRE(5 == root_node_ptr->data);
-        /// 第二层
-        REQUIRE(nullptr == root_node_ptr->left);
-        REQUIRE(nullptr == root_node_ptr->right);
-      }
-    }
-    WHEN("创建一个二叉树, 并插入8个相同的数值函数测试结束时") {
-      THEN("树本身和树节点, 应该被正确析构") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 0);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 0);
-      }
-    }
-    WHEN("创建一个二叉树, 并插入7个不重复的数值, 再插入7个重复数值") {
-      Lee::Binary_Tree<int> insert_tree{5, 2, 7, 1, 3, 6, 8, 4};
-      /// 插入重复数值
-      insert_tree.Insert(5);
-      insert_tree.Insert(2);
-      insert_tree.Insert(7);
-      insert_tree.Insert(1);
-      insert_tree.Insert(3);
-      insert_tree.Insert(6);
-      insert_tree.Insert(8);
-      insert_tree.Insert(4);
-      THEN("这个树被实例化了一次, 节点不为空, 高度为0,且元素个数为8") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 1);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 8);
-        REQUIRE_FALSE(insert_tree.GetRootNode() == nullptr);
-        REQUIRE(3 == insert_tree.GetHeight());
-        REQUIRE(8 == insert_tree.Size());
-      }
-      THEN("树中所有元素的值, 分别为5,2,7,1,3,6,4,8") {
-        auto root_node_ptr = insert_tree.GetRootNode();
-        /// 根节点
-        REQUIRE(5 == root_node_ptr->data);
-        /// 第二层
-        REQUIRE(2 == root_node_ptr->left->data);
-        REQUIRE(7 == root_node_ptr->right->data);
-        /// 第三层
-        REQUIRE(1 == root_node_ptr->left->left->data);
-        REQUIRE(nullptr == root_node_ptr->left->left->left);
-        REQUIRE(nullptr == root_node_ptr->left->left->right);
-        REQUIRE(3 == root_node_ptr->left->right->data);
-        REQUIRE(nullptr == root_node_ptr->left->right->left);
-        REQUIRE(6 == root_node_ptr->right->left->data);
-        REQUIRE(nullptr == root_node_ptr->right->left->left);
-        REQUIRE(nullptr == root_node_ptr->right->left->right);
-        REQUIRE(8 == root_node_ptr->right->right->data);
-        REQUIRE(nullptr == root_node_ptr->right->right->left);
-        REQUIRE(nullptr == root_node_ptr->right->right->right);
-        /// 第四层
-        REQUIRE(4 == root_node_ptr->left->right->right->data);
-        REQUIRE(nullptr == root_node_ptr->left->right->right->left);
-        REQUIRE(nullptr == root_node_ptr->left->right->right->right);
-      }
-    }
-    WHEN("Insert函数重复插入测试结束时") {
-      THEN("树本身和树节点, 应该被正确析构") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 0);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 0);
-      }
-    }
-  }
-
-  GIVEN("测试Find函数") {
-    WHEN("Find函数测试, 在树中找寻特定的值") {
-      Lee::Binary_Tree<int> tree(5);
-      THEN("找寻数值为5的节点是否存在") {
-        REQUIRE_FALSE(nullptr == tree.Find(5));
-        REQUIRE(5 == tree.Find(5)->data);
-      }
-      THEN("找寻不存在的的值, 应该返回 nullptr") {
-        REQUIRE(nullptr == tree.Find(0));
-        REQUIRE(nullptr == tree.Find(6));
-        REQUIRE(nullptr == tree.Find(60));
-        REQUIRE(nullptr == tree.Find(66));
-      }
-      tree.Insert(2);
-      tree.Insert(7);
-      tree.Insert(1);
-      tree.Insert(3);
-      tree.Insert(6);
-      tree.Insert(8);
-      tree.Insert(4);
-      THEN("找寻刚刚根节点的数值, 并验证返回的指针是有效的") {
-        auto node_ptr = tree.Find(5);
-        /// 根节点
-        REQUIRE(5 == node_ptr->data);
-        /// 第二层
-        REQUIRE(2 == node_ptr->left->data);
-        REQUIRE(7 == node_ptr->right->data);
-        /// 第三层
-        REQUIRE(1 == node_ptr->left->left->data);
-        REQUIRE(nullptr == node_ptr->left->left->left);
-        REQUIRE(nullptr == node_ptr->left->left->right);
-        REQUIRE(3 == node_ptr->left->right->data);
-        REQUIRE(nullptr == node_ptr->left->right->left);
-        REQUIRE(6 == node_ptr->right->left->data);
-        REQUIRE(nullptr == node_ptr->right->left->left);
-        REQUIRE(nullptr == node_ptr->right->left->right);
-        REQUIRE(8 == node_ptr->right->right->data);
-        REQUIRE(nullptr == node_ptr->right->right->left);
-        REQUIRE(nullptr == node_ptr->right->right->right);
-        /// 第四层
-        REQUIRE(4 == node_ptr->left->right->right->data);
-        REQUIRE(nullptr == node_ptr->left->right->right->left);
-        REQUIRE(nullptr == node_ptr->left->right->right->right);
-      }
-      THEN("找寻刚刚插入的所有数值是否都能找到") {
-        REQUIRE_FALSE(nullptr == tree.Find(2));
-        REQUIRE(2 == tree.Find(2)->data);
-
-        REQUIRE_FALSE(nullptr == tree.Find(1));
-        REQUIRE(1 == tree.Find(1)->data);
-
-        REQUIRE_FALSE(nullptr == tree.Find(7));
-        REQUIRE(7 == tree.Find(7)->data);
-
-        REQUIRE_FALSE(nullptr == tree.Find(3));
-        REQUIRE(3 == tree.Find(3)->data);
-
-        REQUIRE_FALSE(nullptr == tree.Find(6));
-        REQUIRE(6 == tree.Find(6)->data);
-
-        REQUIRE_FALSE(nullptr == tree.Find(8));
-        REQUIRE(8 == tree.Find(8)->data);
-
-        REQUIRE_FALSE(nullptr == tree.Find(4));
-        REQUIRE(4 == tree.Find(4)->data);
-      }
-    }
-
-    WHEN("Find函数测试结束时") {
-      THEN("树本身和树节点, 应该被正确析构") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 0);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 0);
-      }
-    }
-  }
-
-  GIVEN("Delete函数测试") {
-    WHEN("删除一个树叶（没有子节点），然后再删除一个") {
-      Lee::Binary_Tree<int> delete_tree{5, 2, 7, 1, 3, 6, 8, 4};
-      /// 删除一个树叶
-      delete_tree.Delete(4);
-      THEN("现在的树有七个节点组成， 高度而2") {
-        REQUIRE(Lee::Binary_Tree<int>::construct_count == 1);
-        REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 7);
-        REQUIRE(delete_tree.GetHeight() == 2);
-      }
-      THEN("删除后是一个完全二叉树") {
-        auto node_ptr = delete_tree.GetRootNode();
-        /// root
+      THEN("root不为空指针, root节点的data则为1, 左边和右边都是空指针") {
+        REQUIRE(tree.GetRoot()->left == nullptr);
+        REQUIRE(tree.GetRoot()->right == nullptr);
+        REQUIRE(tree.GetRoot()->data == 1);
+      } /// THEN
+    } /// WHEN
+    WHEN("有多个元素的初值列表") {
+      Lee::Binary_Tree<int> tree{5, 2, 8, 7, 6, 10};
+      THEN("root不为空指针， 一共有六个节点") {
+        /// 验证有几个节点被创建
+        REQUIRE(Lee::Binary_Tree<int>::BinaryTreeNode::construct_count == 6);
+        /// 验证root节点不为空
+        auto node_ptr = tree.GetRoot();
+        REQUIRE(node_ptr != nullptr);
+        /// #1（root）第0层
         REQUIRE(node_ptr->data == 5);
-        /// 第二层
+        /// #2 第1层左节点 （叶子节点）
         REQUIRE(node_ptr->left->data == 2);
-        REQUIRE(node_ptr->right->data == 7);
-        /// 第三层
-        REQUIRE(node_ptr->left->left->data == 1);
-        REQUIRE(node_ptr->left->right->data == 3);
-        REQUIRE(node_ptr->right->left->data == 6);
-        REQUIRE(node_ptr->right->right->data == 8);
-        /// 第四层全部为空
-        REQUIRE(node_ptr->left->left->left == nullptr);
-        REQUIRE(node_ptr->left->left->right == nullptr);
-        REQUIRE(node_ptr->left->right->left == nullptr);
-        REQUIRE(node_ptr->left->right->right == nullptr);
-        REQUIRE(node_ptr->right->left->left == nullptr);
-        REQUIRE(node_ptr->right->left->right == nullptr);
+        /// #3 第1层右节点
+        REQUIRE(node_ptr->right->data == 8);
+        /// #4 第2层左节点 (#3 的左节点)
+        REQUIRE(node_ptr->right->left->data == 7);
+        /// #5 第2层右节点 （叶子节点）(#3 的右节点)
+        REQUIRE(node_ptr->right->right->data == 10);
+        /// #6 第3层左节点 （叶子节点）（#4 的左节点）
+        REQUIRE(node_ptr->right->left->left->data == 6);
+
+        /// #2 叶子节点的验证
+        REQUIRE(node_ptr->left->left == nullptr);
+        REQUIRE(node_ptr->left->right == nullptr);
+        /// #5 叶子节点的验证
         REQUIRE(node_ptr->right->right->left == nullptr);
         REQUIRE(node_ptr->right->right->right == nullptr);
-      }
-      //   /// 删除一个拥有两个节点的节点
-      //   delete_tree.Delete(2);
-      //   THEN("拥有六个节点, 高度仍为2") {
-      //     REQUIRE(Lee::Binary_Tree<int>::construct_count == 1);
-      //     REQUIRE(Lee::BinaryTreeNode<int>::construct_count == 6);
-      //     REQUIRE(delete_tree.GetHeight() == 2);
-      //   }
-      //   THEN("接下来判断树节点的有效性") {
-      //     auto node_ptr = delete_tree.GetRootNode();
-      //     /// root
-      //     REQUIRE(node_ptr->data == 5);
-      //     /// 第二层
-      //     REQUIRE(node_ptr->left->data == 3);
-      //     REQUIRE(node_ptr->right->data == 7);
-      //     /// 第三层
-      //     REQUIRE(node_ptr->left->left->data == 1);
-      //     REQUIRE(node_ptr->left->right == nullptr);
-      //     REQUIRE(node_ptr->right->left->data == 6);
-      //     REQUIRE(node_ptr->right->right->data == 8);
-      //     /// 第四层全部为空
-      //     REQUIRE(node_ptr->left->left->left == nullptr);
-      //     REQUIRE(node_ptr->left->left->right == nullptr);
-      //     REQUIRE(node_ptr->right->left->left == nullptr);
-      //     REQUIRE(node_ptr->right->left->right == nullptr);
-      //     REQUIRE(node_ptr->right->right->left == nullptr);
-      //     REQUIRE(node_ptr->right->right->right == nullptr);
-      //   }
-    }
-  }
-}
+        /// #6 叶子节点的验证
+        REQUIRE(node_ptr->right->left->left->left == nullptr);
+        REQUIRE(node_ptr->right->left->left->right == nullptr);
+        ///// #4 节点的右子树为空的验证
+        REQUIRE(node_ptr->right->left->right == nullptr);
+      } /// THEN
+    } /// WHEN
+    REQUIRE(Lee::Binary_Tree<int>::BinaryTreeNode::construct_count == 0);
+    WHEN("零个元素的初值列表")
+    {
+      Lee::Binary_Tree<int> tree{};
+      THEN("会默认构造一个数值为0的根节点")
+      {
+        REQUIRE(Lee::Binary_Tree<int>::BinaryTreeNode::construct_count == 1);
+        REQUIRE(tree.GetRoot() != nullptr);
+        REQUIRE(tree.GetRoot()->data == 0);
+      } /// THEN
+    } /// WHEN
+    REQUIRE(Lee::Binary_Tree<int>::BinaryTreeNode::construct_count == 0);
+  } /// GIVEN
+} /// SCENARIO
