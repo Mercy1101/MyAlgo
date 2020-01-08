5b51a3bb56c413a8560beec3791b9e8620616552
Mercy1101 <pipinstall@163.com>
2020-01-08 16:19:43 +0800

    整理了格式

--- unittest/tools/profiler/profiler_unittest.cpp
+++ unittest/tools/profiler/profiler_unittest.cpp
@@ -3,10 +3,8 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("PROFILER_F 的基准测试", "[profiler]") {
-  BENCHMARK("简单测试")
-  {
-    for (int i = 0; i < 10; ++i)
-    {
+  BENCHMARK("简单测试") {
+    for (int i = 0; i < 10; ++i) {
       PROFILER_F();
     }
   };
