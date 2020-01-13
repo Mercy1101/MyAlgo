bf35e8fa95b87a48dec3390d2a54e564ae7b63c8
Mercy1101 <pipinstall@163.com>
2020-01-13 08:18:38 +0800

    修正了system_utility模块中关于宽字符和string转换的函数，坑惨我了

--- include/tools/utility/detail/system_utility.h
+++ include/tools/utility/detail/system_utility.h
@@ -15,6 +15,8 @@
 #ifndef MYALGO_INCLUDE_TOOLS_UTILITY_DETAIL_SYSTEM_UTILITY_H_
 #define MYALGO_INCLUDE_TOOLS_UTILITY_DETAIL_SYSTEM_UTILITY_H_
 
+#pragma warning(disable : 4127)
+
 #include <Windows.h>
 #include <crtdbg.h>   // for _CrtDumpMemoryLeaks()
 #include <direct.h>   // for _getcwd()
@@ -22,6 +24,7 @@
 #include <io.h>       /// for _access()
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <cassert>
 #include <iostream>
 #include <string>
@@ -57,17 +60,66 @@ inline bool IsFileExist(const std::string& strFilePathAndName) {
 /// @date     2019-11-22 09:52:06
 /// @warning  线程安全
 /// @note     注意：不可以使用空字符串传入，会抛出异常
-// inline LPCWSTR StringToLPCWSTR(const std::string& strText) {
-//  assert(!strText.empty());
-//  size_t origsize = strText.length() + 1;
-//  const size_t newsize = 100;
-//  size_t convertedChars = 0;
-//  wchar_t* wcstring =
-//      (wchar_t*)malloc(sizeof(wchar_t) * (strText.length() - 1));
-//  mbstowcs_s(&convertedChars, wcstring, origsize, strText.c_str(), _TRUNCATE);
-//
-//  return wcstring;
-//}
+/// inline LPCWSTR StringToLPCWSTR(const std::string& strText) {
+///   // assert(!strText.empty());
+///   size_t origsize = strText.length() + 1;
+///   const size_t newsize = 100;
+///   size_t convertedChars = 0;
+///   wchar_t* wcstring =
+///       (wchar_t*)malloc(sizeof(wchar_t) * (strText.length() - 1));
+///   mbstowcs_s(&convertedChars, wcstring, origsize, strText.c_str(),
+///   _TRUNCATE);
+///
+///   return wcstring;
+/// }
+
+/// @name     StringToWSTR
+/// @brief    std::string 向std::wstring的转换
+///
+/// @param    strText [in]
+///
+/// @return   转换后的std::wstring
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2019-11-22 09:52:06
+/// @warning  线程不安全
+inline std::wstring StringToWSTR(const std::string& s) {
+  int length = static_cast<int>(s.length()) + 1;
+  int len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), length, 0, 0);
+  wchar_t* buf = new wchar_t[len];
+  MultiByteToWideChar(CP_ACP, 0, s.c_str(), length, buf, len);
+  std::wstring r(buf);
+  delete[] buf;
+  return r;
+}
+
+/// @name     Wchar_tToString
+/// @brief    wchar_t to string
+///
+/// @param    szDst    [in]
+/// @param    wchar    [out]
+///
+/// @return   NONE
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2019-11-22 10:09:35
+/// @warning  线程不安全
+/// inline void Wchar_tToString(std::string& szDst, wchar_t* wchar) {
+///   wchar_t* wText = wchar;
+///   DWORD dwNum = WideCharToMultiByte(CP_OEMCP, NULL, wText, -1, NULL, 0, NULL,
+///                                     FALSE);  // WideCharToMultiByte的运用
+///   char* psText;  // psText为char*的临时数组，作为赋值给std::string的中间变量
+///   psText = new char[dwNum];
+///   WideCharToMultiByte(CP_OEMCP, NULL, wText, -1, psText, dwNum, NULL,
+///                       FALSE);  // WideCharToMultiByte的再次运用
+///   szDst = psText;              // std::string赋值
+///   delete[] psText;             // psText的清除
+/// }
+inline void Wchar_tToString(std::string& szDst, wchar_t* wchar) {
+  std::wstring ws(wchar);
+  std::string temp(ws.begin(), ws.end());
+  szDst = temp;
+}
 
 /// @name     GetRootPath
 /// @brief    获取当前程序所在的根目录.(windows系统限定)
@@ -83,14 +135,17 @@ inline std::string GetRootPath() {
   static std::once_flag InstanceFlag;
   static std::string strPathName;
   std::call_once(InstanceFlag, [&]() {
-    char szPath[MAX_PATH] = {0};
+    TCHAR szPath[MAX_PATH] = {0};
     if (!GetModuleFileName(NULL, szPath, MAX_PATH)) {
       std::cout << "Cannot GetModuleFileName!Error Code: " << GetLastError()
                 << std::endl;
-      assert(false && "Cannot GetModuleFileName!");
+      /// assert(false && "Cannot GetModuleFileName!");
+      std::cout << "Cannot GetModuleFileName!" << std::endl;
       strPathName = "";
     }
-    std::string Path(szPath);
+    std::string Path;
+    Wchar_tToString(Path, szPath);
+
     auto pos = Path.find_last_of("\\/");
     strPathName = Path.substr(0, pos);
   });
@@ -115,21 +170,55 @@ inline std::string GetRootPath() {
 ///           如果上一级目录也不存在则需要先创建上一级目录,创建二级目录.
 inline bool CreateFileFolder(const std::string& strFolderPath) {
   if (strFolderPath.empty()) return false;
-  return 0 == CreateDirectory(strFolderPath.c_str(), NULL) ? false : true;
+  auto pwchar = StringToWSTR(strFolderPath).c_str();
+  bool result = true;
+  if (0 == CreateDirectory(pwchar, NULL)) {
+    result = false;
+  }
+  return result;
 }
-#if 0
-#ifdef _DEBUG
-#define new new (_NORMAL_BLOCK, __FILE__, __LINE__)
-    /** CHECK_MEMORY_LEAK这个宏定义写在main函数的第一行,
-     * 有内存泄漏时,VS里面的输出一栏会有内存泄露的打印信息.这个宏只在Debug模式下有用
-     */
-#define CHECK_MEMORY_LEAK()                                                    \
-  _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG)); \
-  _CrtDumpMemoryLeaks()
-#else
-#define CHECK_MEMORY_LEAK() nullptr
-#endif
-#endif
+/// @name     StringToWstring
+/// @brief    string to wstring
+///
+/// @param    szDst    [out]
+/// @param    str      [in]
+///
+/// @return   NONE
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2019-11-22 10:16:52
+/// @warning  线程不安全
+inline void StringToWstring(std::wstring& szDst, const std::string& str) {
+  // std::string temp = str;
+  int len = MultiByteToWideChar(CP_ACP, 0, (LPCSTR)str.c_str(), -1, NULL, 0);
+  wchar_t* wszUtf8 = new wchar_t[len + 1];
+  memset(wszUtf8, 0, len * 2 + 2);
+  MultiByteToWideChar(CP_ACP, 0, (LPCSTR)str.c_str(), -1, (LPWSTR)wszUtf8, len);
+  szDst = wszUtf8;
+  // std::wstring r = wszUtf8;
+  delete[] wszUtf8;
+}
+
+/// @name     ConvertSymbol
+/// @brief    转换路径中的符号，反斜杠转为正斜杠
+///
+/// @param    path  [in/out]
+///
+/// @return   NONE
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2019-12-30 13:13:41
+/// @warning  线程不安全
+inline void ConvertSymbol(std::string* path) {
+  while (true) {
+    auto postion = path->find("\\");
+    if (postion != std::string::npos) {
+      path->replace(postion, 1, "/");
+    } else {
+      return;
+    }
+  }
+}
 
 }  // namespace system
 }  // namespace utility
