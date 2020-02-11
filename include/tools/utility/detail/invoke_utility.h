57c6fe1785a503fa6bf16ecff657769c3b82cbf2
Mercy1101 <pipinstall@163.com>
2020-02-11 11:02:14 +0800

    修改了模板相关的代码,和为一个文件

--- include/tools/utility/detail/invoke_utility.h
+++ include/tools/utility/detail/invoke_utility.h
@@ -5,7 +5,7 @@
 
 ///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
 /// @file   invoke_utility.h
-/// @brief  对调用函数进行了封装, 使用了完美转发
+/// @brief  
 ///
 /// @author lijiancong, pipinstall@163.com
 ///         Taken from <C++ Templates> 2e
@@ -24,21 +24,7 @@ namespace Lee {
 inline namespace utility {
 inline namespace invoke {
 
-template <typename Callable, typename... Args>
-decltype(auto) call(Callable&& op, Args&&... args) {
-  if constexpr (std::is_same_v<std::invoke_result_t<Callable, Args...>, void>) {
-    // return type is void:
-    std::invoke(std::forward<Callable>(op), std::forward<Args>(args)...);
-    //...
-    return;
-  } else {
-    // return type is not void:
-    decltype(auto) ret{
-        std::invoke(std::forward<Callable>(op), std::forward<Args>(args)...)};
-    //...
-    return ret;
-  }
-}
+
 }  // namespace invoke
 }  // namespace utility
 }  // namespace Lee
