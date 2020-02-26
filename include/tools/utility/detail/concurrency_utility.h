f68d1f9121a30032f9150fab0dd9fab1cfa324ea
Mercy1101 <pipinstall@163.com>
2020-02-26 10:22:41 +0800

    添加std::find 的并行实现

--- include/tools/utility/detail/concurrency_utility.h
+++ include/tools/utility/detail/concurrency_utility.h
@@ -516,6 +516,46 @@ void parallel_for_each(Iterator first, Iterator last, Func f) {
   }
 }
 
+/// @name     parallel_find
+/// @brief    std::find 的并行实现
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2020-02-26 10:13:53
+/// @warning  线程不安全
+template <typename Iterator, typename MatchType>
+Iterator parallel_find_impl(Iterator first, Iterator last, MatchType match,
+                            std::atomic<bool>& done) {
+  try {
+    unsigned long const length = std::distance(first, last);
+    unsigned long const min_per_thread = 25;
+    if (length < (2 * min_per_thread)) {
+      for (; (first != last) && !done.load(); ++first) {
+        if (*first == match) {
+          done = true;
+          return first;
+        }
+      }
+      return last;
+    } else {
+      Iterator const mid_point = first + (length / 2);
+      std::future<Iterator> async_result =
+          std::async(&parallel_find_impl<Iterator, MatchType>, mid_point, last,
+                     match, std::ref(done));
+      Iterator const direct_result =
+          parallel_find_impl(first, mid_point, match, done);
+      return (direct_result == mid_point) ? async_result.get() : direct_result;
+    }
+  } catch (...) {
+    done = true;
+    throw;
+  }
+}
+
+template <typename Iterator, typename MatchType>
+Iterator parallel_find(Iterator first, Iterator last, MatchType match) {
+  std::atomic<bool> done(false);
+  return parallel_find_impl(first, last, match, done);
+}
 
 }  // namespace concurrency
 }  // namespace utility
