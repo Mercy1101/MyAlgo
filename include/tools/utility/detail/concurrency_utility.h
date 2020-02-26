49a7c5d9f87c810d39ec64131ca63cd605ac9fac
Mercy1101 <pipinstall@163.com>
2020-02-26 16:35:07 +0800

    添加了一个使用工作窃取的线程池

--- include/tools/utility/detail/concurrency_utility.h
+++ include/tools/utility/detail/concurrency_utility.h
@@ -13,6 +13,7 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <deque>
 #include <functional>
 #include <future>
 #include <list>
@@ -24,6 +25,7 @@
 #include <thread>
 #include <utility>
 #include <vector>
+
 
 namespace Lee {
 inline namespace utility {
@@ -683,6 +685,140 @@ class function_wrapper {
   function_wrapper(const function_wrapper&) = delete;
   function_wrapper(function_wrapper&) = delete;
   function_wrapper& operator=(const function_wrapper&) = delete;
+};
+
+
+class work_stealing_queue {
+ private:
+  using data_type = function_wrapper;
+  std::deque<data_type> the_queue;
+  mutable std::mutex the_mutex;
+
+ public:
+  work_stealing_queue() {}
+  work_stealing_queue(const work_stealing_queue& other) = delete;
+  work_stealing_queue& operator=(const work_stealing_queue& other) = delete;
+
+  void push(data_type data) {
+    std::lock_guard<std::mutex> lock(the_mutex);
+    the_queue.push_front(std::move(data));
+  }
+
+  bool empty() const {
+    std::lock_guard<std::mutex> lock(the_mutex);
+    return the_queue.empty();
+  }
+
+  bool try_pop(data_type& res) {
+    std::lock_guard<std::mutex> lock(the_mutex);
+    if (the_queue.empty()) {
+      return false;
+    }
+
+    res = std::move(the_queue.front());
+    the_queue.pop_front();
+    return true;
+  }
+
+  bool try_steal(data_type& res) {
+    std::lock_guard<std::mutex> lock(the_mutex);
+    if (the_queue.empty()) {
+      return false;
+    }
+
+    res = std::move(the_queue.back());
+    the_queue.pop_back();
+    return true;
+  }
+};
+
+/// @name     thread_pool 
+/// @brief    使用工作窃取的线程池
+///
+/// @author   Lijiancong, pipinstall@163.com
+/// @date     2020-02-26 16:26:49
+/// @warning  线程不安全
+class thread_pool {
+  typedef function_wrapper task_type;
+
+  std::atomic_bool done;
+  threadsafe_queue<task_type> pool_work_queue;
+  std::vector<std::unique_ptr<work_stealing_queue>> queues;
+  std::vector<std::thread> threads;
+  join_threads joiner;
+
+  static thread_local work_stealing_queue* local_work_queue;
+  static thread_local unsigned my_index;
+
+  void worker_thread(unsigned my_index_) {
+    my_index = my_index_;
+    local_work_queue = queues[my_index].get();
+    while (!done) {
+      run_pending_task();
+    }
+  }
+
+  bool pop_task_from_from_local_queue(task_type& task) {
+    return local_work_queue && local_work_queue->try_pop(task);
+  }
+
+  bool pop_task_from_pool_queue(task_type& task) {
+    return pool_work_queue.try_pop(task);
+  }
+
+  bool pop_task_from_other_thread_queue(task_type& task) {
+    for (unsigned i = 0; i < queues.size(); ++i) {
+      unsigned const index = (my_index + i + 1) % queues.size();
+      if (queues[index]->try_steal(task)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+ public:
+  thread_pool() : joiner(threads), done(false) {
+    unsigned const thread_count = std::thread::hardware_concurrency();
+    try {
+      for (unsigned i = 0; i < thread_count; ++i) {
+        queues.push_back(
+            std::unique_ptr<work_stealing_queue>(new work_stealing_queue));
+        threads.push_back(std::thread(&thread_pool::worker_thread, this, i));
+      }
+    } catch (...) {
+      done = true;
+      throw;
+    }
+  }
+
+  ~thread_pool() { done = true; }
+
+  template <typename ResultType>
+  using task_handle = std::future<ResultType>;
+
+  template <typename FunctionType>
+  task_handle<std::result_of<FunctionType()>::type> submit(FunctionType f) {
+    typedef std::result_of<FunctionType()>::type result_type;
+
+    std::packaged_task<result_type()> task(f);
+    task_handle<result_type> res(task.get_future());
+    if (local_work_queue) {
+      local_work_queue->push(std::move(task));
+    } else {
+      pool_work_queue.push(std::move(task));
+    }
+    return res;
+  }
+
+  void run_pending_task() {
+    task_type task;
+    if (pop_task_from_local_queue(task) || pop_task_from_pool_queue(task) ||
+        pop_task_from_other_thread_queue(task)) {
+      task();
+    } else {
+      std::this_thread::yield();
+    }
+  }
 };
 
 }  // namespace concurrency
