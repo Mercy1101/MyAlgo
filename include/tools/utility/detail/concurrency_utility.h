cdabad605599578cf72b7e78471d3ca76491c3da
Mercy1101 <pipinstall@163.com>
2020-02-26 14:13:46 +0800

    添加function_wrapper

--- include/tools/utility/detail/concurrency_utility.h
+++ include/tools/utility/detail/concurrency_utility.h
@@ -653,6 +653,38 @@ void parallel_partial_sum(Iterator first, Iterator last) {
                   (num_threads > 1) ? &previous_end_values.back() : 0, 0);
 }
 
+class function_wrapper {
+  struct impl_base {
+    virtual void call() = 0;
+    virtual ~impl_base() {}
+  };
+
+  std::unique_ptr<impl_base> impl;
+  template <typename F>
+  struct impl_type : impl_base {
+    F f;
+    impl_type(F&& f_) : f(std::move(f_)) {}
+    void call() { f(); }
+  };
+
+ public:
+  template <typename F>
+  function_wrapper(F&& f) : impl(new impl_type<F>(std::move(f))) {}
+
+  void call() { impl->call(); }
+
+  function_wrapper(function_wrapper&& other) : impl(std::move(other.impl)) {}
+
+  function_wrapper& operator=(function_wrapper&& other) {
+    impl = std::move(other.impl);
+    return *this;
+  }
+
+  function_wrapper(const function_wrapper&) = delete;
+  function_wrapper(function_wrapper&) = delete;
+  function_wrapper& operator=(const function_wrapper&) = delete;
+};
+
 }  // namespace concurrency
 }  // namespace utility
 }  // namespace Lee
