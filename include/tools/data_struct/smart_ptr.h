5eb4cba22d6a7a119392c22526ec4d519639c815
Mercy1101 <pipinstall@163.com>
2020-05-14 16:58:18 +0800

    为智能指针添加引用计数

--- include/tools/data_struct/smart_ptr.h
+++ include/tools/data_struct/smart_ptr.h
@@ -24,19 +24,51 @@ inline namespace smart_ptr {
 template <typename T>
 class smart_ptr {
  public:
-  explicit smart_ptr(T* ptr = nullptr) : ptr_(ptr) {}
-  ~smart_ptr() { delete ptr_; }
+  explicit smart_ptr(T* ptr = nullptr) : ptr_(ptr) {
+    if (ptr) {
+      shared_count_ = new shared_count();
+    }
+  }
+
+  smart_ptr(const smart_ptr& other) {
+    ptr_ = other.ptr_;
+    if (ptr_) {
+      other.shared_count_->add_count();
+      shared_count_ = other.shared_count_;
+    }
+  }
+
+  template <typename U>
+  smart_ptr(const smart_ptr<U>& other) {
+    ptr_ = other.ptr_;
+    if (ptr_) {
+      other.shared_count_->add_count();
+      shared_count_ = other.shared_count_;
+    }
+  }
+
+  /// 这里引入模板U, 是为了自动类型推导来支持子类指针向基类指针的转换
+  template <typename U>
+  smart_ptr(const smart_ptr<U>&& other) {
+    ptr_ = other.ptr_;
+    if (ptr_) {
+      shared_count_ = other.shared_count_;
+      other.ptr = nullptr;
+    }
+  }
+
+  ~smart_ptr() {
+    if (nullptr != ptr_ && !shared_count_()->reduce_count()) {
+      delete ptr_;
+      delete shared_count_;
+    }
+    delete ptr_;
+  }
 
   T* get() const { return ptr_; }
   T& operator*() const { return *ptr_; }
   T* operator->() const { return ptr_; }
   operator bool() const { return ptr_; }
-
-  /// 子类指针向基类指针的转换
-  template <typename U>
-  smart_ptr(smart_ptr<U>&& other) {
-    ptr = other.release();
-  }
 
   smart_ptr& operator=(smart_ptr rhs) {
     smart_ptr(rhs).swap(*this);
@@ -52,10 +84,31 @@ class smart_ptr {
   void swap(smart_ptr& rhs) {
     using std::swap;
     swap(ptr_, rhs.ptr_);
+    swap(shared_count_, rhs.shared_count_);
   }
 
  private:
+  class shared_count {
+   public:
+    shared_count() : count_(-1) {}
+
+    void add_count() { ++count_; }
+    long reduce_count() { return --count_; }
+    long get_count() const { return count_; }
+    long use_count() const {
+      if (ptr_) {
+        return shared_count_->get_count_();
+      } else {
+        return 0;
+      }
+    }
+
+   private:
+    long count_;
+  };
+
   T* ptr_;
+  shared_count* shared_count_;
 };
 
 }  // namespace smart_ptr
