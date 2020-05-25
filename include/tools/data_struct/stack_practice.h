505cdf5df05bac36a41a16c102ccea60e78b871e
lijiancong <18221336269@163.com>
2020-05-25 13:03:01 +0800

    把运算行为写出来了，接下来考虑一下运算优先级

--- include/tools/data_struct/stack_practice.h
+++ include/tools/data_struct/stack_practice.h
@@ -52,20 +52,66 @@ class simple_calculate {
     }
 
     std::stack<int> number_stack;
-    std::stack<int> operator_stack;
-    auto old_iterator = parsed_string.cbegin();
-    for (auto& c = parsed_string.cbegin(); c != parsed_string.cend();
-         ++c) {
-      if (*c != '+' && *c != '-' && *c != '*' && *c != '/') {
-        std::string temp(old_iterator, c);
-        number_stack.push(*c);
-      } else {
-        
-      }
+    std::stack<char> operator_stack;
+
+    /// 提取数字
+    auto start_index = 0;
+    while (auto current_index = parsed_string.find_first_of(
+                                    "+-*/", start_index) != std::string::npos) {
+      auto num_string = parsed_string.substr(start_index, current_index);
+      number_stack.push(std::stoi(num_string));
+      operator_stack.push(parsed_string.at(current_index));
+      start_index += current_index + 1;
     }
+    /// 提取最后一个数字
+    number_stack.push(
+        std::stoi(parsed_string.substr(start_index, parsed_string.size())));
+    if (number_stack.size() != operator_stack.size() + 1) {
+      throw std::exception("invalid number!");
+    }
+
+    /// 开始计算结果
+    int result = 0;
+    auto first_number = number_stack.pop();
+    result += calculate_number(first_number, number_stack.pop(),
+                               operator_stack.pop());
+    while (auto it = operator_stack.pop()) {
+      auto temp_result = number_stack.pop();
+      resullt += calculate_number(result, temp_result, it);
+    }
+    return result;
   }
 
  private:
+  /// @name     calculate_number
+  /// @brief    计算结果并返回，遇到错误抛出异常
+  ///
+  /// @param    first_number    [in]  第一个运算数字
+  /// @param    seconde_number  [in]  第二个运算数字
+  /// @param    op              [in]  运算符
+  ///
+  /// @return   运算的结果
+  ///
+  /// @author   Lijiancong, pipinstall@163.com
+  /// @date     2020-05-25 12:54:38
+  /// @warning  线程不安全
+  int calculate_number(const int first_number, const int second_number,
+                       const char op) {
+    if ('+' == op) {
+      return first_number + second_number;
+    } else if ('-' == op) {
+      return first_number - second_number;
+    } else if ('*' == op) {
+      return first_number * second_number;
+    } else if ('/' == op) {
+      if (second_number == 0) {
+        throw std::exception("div zero error!");
+      }
+      return first_number / second_number;
+    } else {
+      throw std::exception("unkonw operator!");
+    }
+  }
   /// @name     parse_expression
   /// @brief    解析输入字符串，去除了空格
   ///
@@ -79,8 +125,8 @@ class simple_calculate {
   std::string parse_expression(const std::string& expression) {
     std::string parsed_string;
 
-    while ((int index = parsed_string.find(' ')) != std::string::npos) {
-       parsed_string.erase(index, 1);
+    while (int index = parsed_string.find(' ') != std::string::npos) {
+      parsed_string.erase(index, 1);
     }
     return parsed_string;
   }
@@ -107,7 +153,6 @@ class simple_calculate {
     }
     return true;
   }
-
 }
 
 }  // namespace stack_practice
