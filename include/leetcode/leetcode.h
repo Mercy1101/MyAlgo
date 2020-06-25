1538d81d9d96aa6840b4e8142933fa7802402038
lijiancong <18221336269@163.com>
2020-06-25 22:37:48 +0800

    刷题ing

--- include/leetcode/leetcode.h
+++ include/leetcode/leetcode.h
@@ -1271,22 +1271,48 @@ inline ListNode *removeNthFromEnd(ListNode *head, int n) {
   return head;
 }
 
-/// 673. 最长递增子序列的个数
-/// 给定一个未排序的整数数组，找到最长递增子序列的个数。
-///
-/// 示例 1:
-///
-/// 输入: [1,3,5,4,7]
-/// 输出: 2
-/// 解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
-/// 示例 2:
-///
-/// 输入: [2,2,2,2,2]
-/// 输出: 5
-/// 解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
-/// 注意: 给定的数组长度不超过 2000 并且结果一定是32位有符号整数。
-inline int findNumberOfLIS(std::vector<int> &nums) { (void)nums; }
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+  ListNode *root = new ListNode(-1);
+  ListNode *temp = root;
+  while (l1 != nullptr && l2 != nullptr) {
+    if (l1->val > l2->val) {
+      root->next = l2;
+      l2 = l2->next;
 
+    } else {
+      root->next = l1;
+      l1 = l1->next;
+    }
+    root = root->next;
+  }
+  if (l1 == nullptr) {
+    root->next = l2;
+  } else {
+    root->next = l1;
+  }
+
+  return temp->next;
+}
+
+std::vector<std::vector<int>> levelOrder(TreeNode* root) {
+        if(!root) return {};
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.size()) {
+            int len = q.size();
+            vector<int> level;
+            for (int i = 0; i < len; i ++) {
+                TreeNode* t = q.front();
+                q.pop();
+                level.push_back(t->val);
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            res.push_back(level);
+        }
+        return res;
+    }
 }  // namespace leetcode
 }  // namespace lee
 
