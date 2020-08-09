511193a9c095328daa61481381072cfdae7f28c1
lijiancong <pipinstall@163.com>
2020-08-09 16:11:17 +0800

    编译通过

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,32 @@
 ﻿#define CATCH_CONFIG_RUNNER
+#include <gperftools/profiler.h>
+
 #include <catch2/catch.hpp>  // include after defining CATCH_CONFIG_RUNNER
 #include <iostream>
 
 #include "utility/utility.h"
 
-/// int main(int argc, char** argv) {
-///   PrintVersion();
-///   CheckConfigFolder();
-///   Catch::Session().run(argc, argv);
-/// 
-///   return 0;
+int main(int argc, char** argv) {
+  ProfilerStart("main");
+  Catch::Session().run(argc, argv);
+  ProfilerStop();
+  return 0;
+}
+/// #include <benchmark/benchmark.h>
+///
+/// static void BM_StringCreation(benchmark::State& state) {
+///   for (auto _ : state)
+///     std::string empty_string;
 /// }
-#include <benchmark/benchmark.h>
-
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
-
-BENCHMARK_MAIN();
+/// // Register the function as a benchmark
+/// BENCHMARK(BM_StringCreation);
+///
+/// // Define another benchmark
+/// static void BM_StringCopy(benchmark::State& state) {
+///   std::string x = "hello";
+///   for (auto _ : state)
+///     std::string copy(x);
+/// }
+/// BENCHMARK(BM_StringCopy);
+///
+/// BENCHMARK_MAIN();
