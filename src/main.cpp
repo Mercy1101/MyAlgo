3d522b98a342e9e741df5d2113996bbb7db543d8
lijiancong <18221336269@163.com>
2020-09-22 21:37:11 +0800

    添加单元测试文件

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,28 @@
 #include <catch2/catch.hpp>  // include after defining CATCH_CONFIG_RUNNER
 #include <iostream>
 
+#include "ut.hpp"
 #include "utility/utility.h"
 
 int main(int argc, char** argv) {
-  Catch::Session().run(argc, argv);
+  lee::ignore_unused(argc, argv);
+  /// Catch::Session().run(argc, argv);
+  boost::ut::expect(true);
+  boost::ut::expect(1 == 2);
+  using namespace boost::ut;
+  expect(1_i == 2);
+  using namespace boost::ut::literals;
+  using namespace boost::ut::operators::terse;
+
+  expect(1_i == 2);                  // UDL syntax
+  expect(1 == 2_i);                  // UDL syntax
+  expect(that % 1 == 2);             // Matcher syntax
+  expect(eq(1, 2));                  // eq/neq/gt/ge/lt/le
+  "hello world"_test = [] {
+    int i = 43;
+    expect(42_i == i);
+  };
+  system("pause");
   return 0;
 }
 /// #include <benchmark/benchmark.h>
