b9e26b5121f24bd1248555df5493b83a391815ed
lijiancong <18221336269@163.com>
2020-10-02 17:37:17 +0800

    添加单元测试初始化

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,11 @@
-﻿#include <iostream>
+﻿#define CATCH_CONFIG_RUNNER
 
+#include <iostream>
+
+#include "catch/catch.hpp"  // include after defining CATCH_CONFIG_RUNNER
 #include "my_algo.hpp"
 
-int main() { system("pause"); }
+int main(int argc, char** argv) {
+  Catch::Session().run(argc, argv);
+  return 0;
+}
