f167f3141f76fd38e3af4cc583d392506f52c4d8
lijiancong <18221336269@163.com>
2020-10-02 14:39:46 +0800

    添加my_algo文件

---
+++ include/my_algo.hpp
@@ -0,0 +1,29 @@
+﻿///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+///
+/// Use of this source code is governed by a MIT license
+/// that can be found in the License file.
+///
+/// @file   my_algo.hpp
+/// @brief  工具集
+///
+/// @author lijiancong, pipinstall@163.com
+/// @date   2020-10-02 09:33:48
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+#ifndef INCLUDE_MY_ALGO_HPP_
+#define INCLUDE_MY_ALGO_HPP_
+
+#include "detail/allocator.hpp"
+#include "detail/concurrency.hpp"
+#include "detail/detail.hpp"
+#include "detail/file.hpp"
+#include "detail/file_line_reader_utility.hpp"
+#include "detail/lazy.hpp"
+#include "detail/lru_cache.hpp"
+#include "detail/math.hpp"
+#include "detail/random.hpp"
+#include "detail/time.hpp"
+#include "detail/union_find.hpp"
+
+#endif
