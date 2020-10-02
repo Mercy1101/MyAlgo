2ed268c19a7c9b147500820c6c5c89438a568bd6
lijiancong <18221336269@163.com>
2020-10-02 17:45:08 +0800

    添加multi_s()单元测试

---
+++ unittest/multi_s_unittest.cc
@@ -0,0 +1,17 @@
+﻿#include "catch/catch.hpp"
+#include "my_algo.hpp"
+
+TEST_CASE("multi_s() 简单测试", "[lee][multi_s]") {
+  using namespace lee;
+
+  auto xx0 = multi_s(2, 4);
+  REQUIRE(xx0.has_value());
+  REQUIRE(xx0.value() == 2 * 4);
+
+  auto xx1 = multi_s(INT_MIN, -1);
+  REQUIRE_FALSE(xx1.has_value());
+
+  auto xx2 = multi_s(INT_MIN / 2, 3);
+  REQUIRE_FALSE(xx2.has_value());
+  REQUIRE(xx2.value_or(1) == 1);
+}
