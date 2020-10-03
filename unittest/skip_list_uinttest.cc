f603a02686dcfbd892d42b0a24e411222f27c069
lijiancong <18221336269@163.com>
2020-10-03 10:49:56 +0800

    完成了跳表的简单测试文件

---
+++ unittest/skip_list_uinttest.cc
@@ -0,0 +1,17 @@
+﻿#include "catch/catch.hpp"
+#include "my_algo.hpp"
+
+TEST_CASE("skip_list 简单测试", "[lee][skip_list]") {
+  using namespace lee;
+
+  skip_list skiplist;
+  skiplist.add(1);
+  skiplist.add(2);
+  skiplist.add(3);
+  REQUIRE(!skiplist.search(0));
+  skiplist.add(4);
+  REQUIRE(skiplist.search(1));
+  REQUIRE(!skiplist.erase(0));   // 返回 false，0 不在跳表中
+  REQUIRE(skiplist.erase(1));    // 返回 true
+  REQUIRE(!skiplist.search(1));  // 返回 false，1 已被擦除
+}
