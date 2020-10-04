eed54b4c0a04daf1d9521b099fbc8ad3c5190b67
lijiancong <18221336269@163.com>
2020-10-04 21:30:54 +0800

    完成了所有基本测试用例的补充

--- include/detail/big_int.hpp
+++ include/detail/big_int.hpp
@@ -14,7 +14,12 @@
 #ifndef INCLUDE_DETAIL_BIG_INT_HPP_
 #define INCLUDE_DETAIL_BIG_INT_HPP_
 
+#include <algorithm>
+#include <exception>
+#include <functional>
+#include <limits>
 #include <map>
+#include <numeric>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -23,18 +28,22 @@ namespace lee {
 inline namespace bigint {
 class big_int {
  public:
-  big_int() {}
+  big_int() { num_.push_back(0); }
   big_int(long long i) {
-    positive_ = (i >= 0);
-    if (!positive_ && i != std::numeric_limits<long long>::lowest()) {
-      i *= -1;
-    }
-    while (i != 0) {
-      auto temp = static_cast<int>(i % base_);
-      if (temp < 0) temp *= -1;
-      num_.push_back(temp);
-      i /= base_;
-      if (i < 0) i *= -1;
+    if (i == 0) {
+      num_.push_back(0);
+    } else {
+      positive_ = (i >= 0);
+      if (!positive_ && i != std::numeric_limits<long long>::lowest()) {
+        i *= -1;
+      }
+      while (i != 0) {
+        auto temp = static_cast<int>(i % base_);
+        if (temp < 0) temp *= -1;
+        num_.push_back(temp);
+        i /= base_;
+        if (i < 0) i *= -1;
+      }
     }
   }
 
@@ -42,62 +51,81 @@ class big_int {
       : num_(other.num_), positive_(other.positive_), base_(other.base_) {}
 
   big_int(const std::string &str) {
-    positive_ = str.at(0) != '-';
+    if (str.empty() || str == "0" || str == "-0") {
+      num_.push_back(0);
+    } else {
+      positive_ = str.at(0) != '-';
 
-    int size = static_cast<int>(str.size());
-    while (true) {
-      if (size <= 0) {
+      int size = static_cast<int>(str.size());
+      while (true) {
+        if (size <= 0) {
+          break;
+        }
+        if (!positive_ && size < 1) {
+          throw std::logic_error("invalid param in big_int!");
+        }
+        int length = 0;
+        int num = 0;
+        int prefix = 1;
+        for (int i = size - 1; i >= 0 && i >= size - 9; --i) {
+          if (str.at(i) < '0' || str.at(i) > '9') {
+            if (i == 0 && str.at(i) == '-' && !positive_) {
+              size = 0;
+              break;  ///< 跳过第一个负号
+            } else {
+              throw std::logic_error("invalid param in big_int!");
+            }
+          }
+          num += (str.at(i) - '0') * prefix;
+          prefix *= 10;
+          ++length;
+        }
+        num_.push_back(num);
+        size -= length;
+      }
+    }
+    while (true && num_.size() > 1) {
+      /// 去除头部多余的零
+      if (num_.back() == 0) {
+        num_.pop_back();
+      } else {
         break;
       }
-      if (!positive_ && size <= 1) {
-        throw std::logic_error("invalid param in big_int!");
-      }
-      int length = 0;
-      int num = 0;
-      int prefix = 1;
-      for (int i = size - 1; i >= 0 && i >= size - 9; --i) {
-        if (str.at(i) < '0' || str.at(i) > '9') {
-          if (i == 0 && str.at(i) == '-' && !positive_) {
-            size = 0;
-            break;  ///< 跳过第一个负号
-          } else {
-            throw std::logic_error("invalid param in big_int!");
-          }
-        }
-        num += (str.at(i) - '0') * prefix;
-        prefix *= 10;
-        ++length;
-      }
-      num_.push_back(num);
-      size -= length;
     }
   }
 
-  big_int &operator+(long long i) const {
+  big_int operator+(long long i) const {
     big_int c = *this;
     c += i;
     return c;
   }
 
   big_int &operator+=(long long i) {
-    auto it = num_.begin();
-    if (skip_ > num_.size() - 1) {
-      num_.insert(num_.end(), skip_ - num_.size(), 0);
-    }
-    it += skip_;
-    do {
-      if (it != num_.end()) {
-        *it += i % base_;
-        i /= base_;
-        i += *it / base_;  ///< i 加上进位
-        *it %= base_;      ///< 当前值消除进位
-        ++it;
-      } else {
-        num_.push_back(0);  ///< 添加一位，下一个循环再赋值
-        it = std::prev(num_.end());  ///< 更新迭代器为有效状态
+    if ((i >= 0) ^ positive_) {
+      /// 符号不同则计算减法
+      i *= -1;
+      *this -= i;
+      return *this;
+    } else {
+      auto it = num_.begin();
+      if (skip_ > num_.size() - 1) {
+        num_.insert(num_.end(), skip_ - num_.size(), 0);
       }
-    } while (i != 0);
-    return *this;
+      it += skip_;
+      do {
+        if (it != num_.end()) {
+          *it += i % base_;
+          i /= base_;
+          i += *it / base_;  ///< i 加上进位
+          *it %= base_;      ///< 当前值消除进位
+          ++it;
+        } else {
+          num_.push_back(0);  ///< 添加一位，下一个循环再赋值
+          it = std::prev(num_.end());  ///< 更新迭代器为有效状态
+        }
+      } while (i != 0);
+      return *this;
+    }
   }
 
   big_int operator=(const long long &a) {
@@ -111,9 +139,11 @@ class big_int {
     return *this;
   }
   big_int &operator=(const big_int &other) {
-    big_int temp(other.to_string());
-    *this = temp;
-    /// *this<<other;
+    if (&other != this) {
+      base_ = other.base_;
+      num_ = other.num_;
+      positive_ = other.positive_;
+    }
     return *this;
   }
 
@@ -124,15 +154,22 @@ class big_int {
     return c;
   }
 
-  big_int &operator+=(const big_int &b) {
-    if (!b.positive_) {
+  big_int &operator+=(const big_int &temp) {
+    auto b = *this;
+    if (&temp != this) {
+      b = temp;
+    }
+
+    if (positive_ ^ b.positive_) {
+      /// 异号相减
+      b *= -1;
       return *this -= b;
     }
 
     auto it1 = num_.begin();
     auto it2 = b.num_.begin();
     int sum = 0;
-    while (it1 != num_.end() || it2 != num_.end()) {
+    while (it1 != num_.end() || it2 != b.num_.end()) {
       if (it1 != num_.end()) {
         sum += *it1;
       } else {
@@ -153,34 +190,55 @@ class big_int {
     }
     return *this;
   }
+
+  big_int operator-(long long l) const {
+    big_int b(l);
+    big_int c = *this;
+    c -= b;
+    return c;
+  }
+  big_int &operator-=(long long l) {
+    big_int b(l);
+    *this -= b;
+    return *this;
+  }
+
   big_int operator-(const big_int &b) const {
     big_int c = *this;
     c -= b;
-
     return c;
   }
 
-  big_int &operator-=(const big_int &b) {
-    auto it1 = num_.begin();
-    auto it2 = b.num_.begin();
-    int dif = 0;
-    while (it1 != num_.end() || it2 != b.num_.end()) {
-      if (it1 != num_.end()) {
-        dif += *it1;
-        ++it1;
+  big_int &operator-=(const big_int &temp) {
+    auto b = *this;
+    if (&temp != this) {
+      b = temp;
+    }
+    if (temp.positive_ ^ positive_) {
+      b *= -1;
+      /// 异号
+      *this += b;
+    } else {
+      auto it1 = num_.begin();
+      auto it2 = b.num_.begin();
+      int dif = 0;
+      while (it1 != num_.end() || it2 != b.num_.end()) {
+        if (it1 != num_.end()) {
+          dif += *it1;
+          ++it1;
+        }
+        if (it2 != b.num_.end()) {
+          dif -= *it2;
+          ++it2;
+        }
+        if (dif < 0) {
+          *std::prev(it1) = dif + base_;
+          dif = -1;  ///< 向高位借位
+        } else {
+          *std::prev(it1) = dif % base_;
+          dif /= base_;
+        }
       }
-      if (it2 != b.num_.end()) {
-        dif -= *it2;
-        ++it2;
-      }
-      if (dif < 0) {
-        *(it1 - 1) = dif + base_;
-        dif = -1;  ///< 向高位借位
-      } else {
-        *(it1 - 1) = dif % base_;
-        dif /= base_;
-      }
-
       if (dif < 0) positive_ = false;
       while (num_.size() > 1) {
         it1 = num_.end() - 1;
@@ -194,21 +252,25 @@ class big_int {
   }
 
   big_int operator*(big_int const &b) {
-    if (b.num_.size() == 1) return *this *= b.num_[0];
-    std::vector<int>::iterator it1;
-    std::vector<int>::const_iterator it2;
     big_int c;
-    for (it1 = num_.begin(); it1 != num_.end(); ++it1) {
-      for (it2 = b.num_.begin(); it2 != b.num_.end(); ++it2) {
-        auto distance = std::distance(it1, num_.begin()) +
-                        std::distance(it2, b.num_.begin());
-        c.skip_ = static_cast<unsigned>(distance);
-        c += static_cast<long long>(*it1) * (*it2);
+    if (b == big_int(-1)) {
+      positive_ = !positive_;
+      return c;
+    } else {
+      if (b.num_.size() == 1) return *this *= b.num_[0];
+      std::vector<int>::iterator it1;
+      std::vector<int>::const_iterator it2;
+      for (it1 = num_.begin(); it1 != num_.end(); ++it1) {
+        for (it2 = b.num_.begin(); it2 != b.num_.end(); ++it2) {
+          auto distance = std::distance(it1, num_.begin()) +
+                          std::distance(it2, b.num_.begin());
+          c.skip_ = static_cast<unsigned>(distance);
+          c += static_cast<long long>(*it1) * (*it2);
+        }
       }
+      c.skip_ = 0;
+      return c;
     }
-    c.skip_ = 0;
-
-    return c;
   }
 
   big_int &operator*=(const big_int &b) {
@@ -220,54 +282,64 @@ class big_int {
   big_int operator*(const long long &b) {
     big_int c = *this;
     c *= b;
-
     return c;
   }
 
   big_int &operator*=(const long long &b) {
-    std::vector<int>::iterator it = num_.begin();
-    long long sum = 0;
-    while (it != num_.end()) {
-      sum += (long long)(*it) * b;
-      *it = (int)(sum % base_);
-      sum /= base_;
-      ++it;
+    if (b == -1) {
+      positive_ = !positive_;
+      return *this;
+    } else {
+      std::vector<int>::iterator it = num_.begin();
+      long long sum = 0;
+      while (it != num_.end()) {
+        sum += (long long)(*it) * b;
+        *it = (int)(sum % base_);
+        sum /= base_;
+        ++it;
+      }
+      if (sum) num_.push_back((int)sum);
+
+      return *this;
     }
-    if (sum) num_.push_back((int)sum);
-
-    return *this;
   }
 
-  // Power
-  big_int pow(int const &power, std::map<int, big_int> &lookup) {
-    if (power == 1) return *this;
-    if (lookup.count(power)) return lookup[power];
-
-    int closestPower = 1;
-    while (closestPower < power) closestPower <<= 1;
-    closestPower >>= 1;
-
-    if (power == closestPower)
-      lookup[power] = pow(power / 2, lookup) * pow(power / 2, lookup);
-    else
-      lookup[power] =
-          pow(closestPower, lookup) * pow(power - closestPower, lookup);
-
-    return lookup[power];
-  }
-
-  big_int &pow(int const &power) {
+  big_int &pow(const int &power) {
+    if (power < 0) {
+      throw std::logic_error("invalid param in big_int::pow()");
+    }
     std::map<int, big_int> lookup;
+    lookup.insert({0, big_int(1)});
+    lookup.insert({1, *this});
     if (power % 2 == 0 && !positive_) {
       positive_ = true;
     }
-    *this = pow(power, lookup);
+    std::function<big_int(const int)> func = [&, this](const int n) {
+      if (n == 1) return lookup[n];
+      if (lookup.count(n)) return lookup[n];
+
+      int closestPower = 1;
+      while (closestPower < n) closestPower <<= 1;
+      closestPower >>= 1;
+
+      if (n == closestPower)
+        lookup[n] = func(n / 2) * func(n / 2);
+      else
+        lookup[n] = func(closestPower) * func(n - closestPower);
+
+      return lookup[n];
+    };
+    *this = func(power);
 
     return *this;
   }
 
   // 0 this == a || -1 this < a || 1 this > a
   int compare(const big_int &a) const {
+    /// 比较对象是自己
+    if (&a == this) {
+      return 0;
+    }
     /// 异号情况
     if (positive_ && !a.positive_) return 1;
     if (!positive_ && a.positive_) return -1;
@@ -365,23 +437,44 @@ class big_int {
     }
     return out;
   }
+  bool get_positive() { return positive_; }
+
+ private:
+  static constexpr int BASE_NUM_ = static_cast<int>(1e9);
+  std::vector<int> num_;
+  bool positive_ = true;
+  int base_ = BASE_NUM_;
+  unsigned skip_ = 0;
+
   int segment_length(int segment) const {
     int length = 0;
     while (segment != 0) {
       segment /= 10;
       ++length;
     }
-
     return length;
   }
 
- private:
-  static constexpr int BASE_NUM_ = static_cast<int>(1e9);
-  std::vector<int> num_;
-  bool positive_ = true;
-  const int base_ = BASE_NUM_;
-  unsigned skip_ = 0;
-};
+#if false
+// Power
+big_int pow(int const &power, std::map<int, big_int> &lookup) {
+  if (power == 1) return *this;
+  if (lookup.count(power)) return lookup[power];
+
+  int closestPower = 1;
+  while (closestPower < power) closestPower <<= 1;
+  closestPower >>= 1;
+
+  if (power == closestPower)
+    lookup[power] = pow(power / 2, lookup) * pow(power / 2, lookup);
+  else
+    lookup[power] =
+        pow(closestPower, lookup) * pow(power - closestPower, lookup);
+
+  return lookup[power];
+}
+#endif
+};  // namespace bigint
 
 inline lee::big_int factorial(long long n) {
   lee::big_int result = 1;
