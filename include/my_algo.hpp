4b6fefdac65e6a7d41a16faf25622bdb85dc7af6
lijiancong <18221336269@163.com>
2020-10-06 21:24:14 +0800

    添加KMP的实现

--- include/my_algo.hpp
+++ include/my_algo.hpp
@@ -15,10 +15,12 @@
 #define INCLUDE_MY_ALGO_HPP_
 
 #include "detail/allocator.hpp"
+#include "detail/big_int.hpp"
 #include "detail/concurrency.hpp"
 #include "detail/detail.hpp"
 #include "detail/file.hpp"
 #include "detail/file_line_reader_utility.hpp"
+#include "detail/kmp.hpp"
 #include "detail/lazy.hpp"
 #include "detail/lru_cache.hpp"
 #include "detail/math.hpp"
@@ -26,6 +28,5 @@
 #include "detail/skip_list.hpp"
 #include "detail/time.hpp"
 #include "detail/union_find.hpp"
-#include "detail/big_int.hpp"
 
 #endif
