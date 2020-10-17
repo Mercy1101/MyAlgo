80aa6e79c0eba8bf72b2c410d910cc3539fb2b86
lijiancong <18221336269@163.com>
2020-10-17 12:53:37 +0800

    完成二维数组序列化与反序列化的简单测试

---
+++ unittest/two_dimensional_vector_serialize_unittest.cc
@@ -0,0 +1,14 @@
+﻿#include "catch/catch.hpp"
+#include "my_algo/my_algo.hpp"
+
+TEST_CASE("二维数组序列化与反序列化 简单测试",
+          "[lee][two_dimensional_vector]") {
+  using namespace lee;
+  two_dimensional_vector_serialize p;
+  std::vector<std::vector<int>> vec;
+  vec.push_back({1, 2});
+  vec.push_back({3, 4});
+  vec.push_back({5, 6});
+  vec.push_back({7, 8});
+  REQUIRE(vec == p.deserialize(p.serialize(vec)));
+}
